ff4c261b7272de45f2bd26b32a370cf1b791fb97
scvds123645 <270fe019@gotrak.org>
2025-12-05 22:42:24 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/test">
+      <BrowserRouter basename="/zhuye">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
